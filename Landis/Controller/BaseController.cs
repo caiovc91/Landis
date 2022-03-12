e6de80e3c32fe9194df689a28dfc51bfa70878c8
mu4ev <mu4ev@DESKTOP-3Q0822U>
2022-03-12 12:32:38 -0300

    Add project files.

---
+++ Landis/Controller/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Landis.Controller
+{
+    public class BaseController
+    {
+        public void InsertEndpoint(int id, string serial,  )
+        {
+
+            return endpoint;
+        }
+
+        public Endpoint EditEndpoint
+    }
+}
---
+++ Landis/Models/Endpoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Landis
+{
+    public class Endpoint
+    {
+        public int Id { get; set; }
+        public string serial_number { get; set; }
+        public int model_id { get; set; }
+        public int meter_number { get; set; }
+        public string firmware_version { get; set; }
+        public int switch_state { get; set; }
+    }
+}
